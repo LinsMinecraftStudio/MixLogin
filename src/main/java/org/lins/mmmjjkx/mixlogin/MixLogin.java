7795a835b5679a30753f4ea838ed827279cb446c
lijinhong11 <mcjrhugh@163.com>
2023-03-03 19:57:51 +0800

    initial commit

---
+++ src/main/java/org/lins/mmmjjkx/mixlogin/MixLogin.java
@@ -0,0 +1,17 @@
+package org.lins.mmmjjkx.mixlogin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MixLogin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
